71cedde87866d8b294115481972ece8b6f8604b6
thuan6420 <6420startlife@gmail.com>
2022-06-26 02:32:37 +0700

    Day 1 - design Login UI

---
+++ app/src/main/java/com/ptithcm/thuan6420/chatapplication/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.ptithcm.thuan6420.chatapplication.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.ptithcm.thuan6420.chatapplication.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
